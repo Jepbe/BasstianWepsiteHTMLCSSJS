f6be74bb2e741636f96ff1b1cd7fd164b5699135
Jeppe B. Bech <67188645+Jepbe@users.noreply.github.com>
2022-09-20 19:31:57 +0200

    loats of shit

--- script/main.js
+++ script/main.js
@@ -5,4 +5,23 @@ function unfoldMusic() {
     } else {
         x.style.display = "block";
     }
+
+    var y = document.getElementById("btnDiv");
+    if (y.style.bottom === "5rem") {
+        y.style.bottom = "3rem";
+    } else {
+        y.style.bottom = "5rem"
+    }
+
+    // var z = document.getElementById("button");
+    // if (z.style.transform = "rotate(270deg)") {
+    //     z.style.transform = "rotate(180deg)";
+    // } 
+    // if (z.style.transform = "rotate(90deg)"); {
+    //     z.style.transform = "rotate(180deg)"
+    // }   
 }
+
+
+// links
+
