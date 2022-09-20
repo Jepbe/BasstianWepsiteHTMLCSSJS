2a85680e629a44ad310d313fdfc58a1cea222b43
Jeppe B. Bech <67188645+Jepbe@users.noreply.github.com>
2022-09-20 11:49:45 +0200

    qwe

--- script/main.js
+++ script/main.js
@@ -1,10 +1,8 @@
-
-
-var btn = document.querySelector("#button");
-document.querySelector("#button").addEventListener("click", unfoldMusic);
-
 function unfoldMusic() {
-    var bBtn = document.querySelector("#bottomBtn");
-    bBtn.style.backgroundColor = "transparent";
-    console.log("works")
+    var x = document.getElementById("bottomBtn");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
