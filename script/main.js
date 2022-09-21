81445e03c64d0c5ca10aaed701220c78702eb63d
Jeppe B. Bech <67188645+Jepbe@users.noreply.github.com>
2022-09-21 12:21:03 +0200

    mer eller mnindre done

--- script/main.js
+++ script/main.js
@@ -37,3 +37,36 @@ function buyT() {
 function book() {
     window.location.href = "/kontakt"
 }
+
+function tiktok() {
+    window.open( 
+        "https://www.tiktok.com/@dj_basstian", "_blank" );
+}
+
+function soundcloud() {
+    window.open(
+        "https://soundcloud.com/bastian-nielsen-360664195", "_blank");
+}
+function instagram() {
+    window.open(
+        "https://www.instagram.com/dj_basstian/", "_blank");
+}
+function youtube() {
+    window.open(
+        "https://www.youtube.com/channel/UCFjgyN5rCfz8SNGd56Lyp3Q", "_blank");
+}
+function homePage() {
+    window.location.href = "/";
+}
+
+
+// Open navbar
+
+function navbarOpen() {
+    var list = document.getElementById("navList");
+    if (list.style.display === "block") {
+        list.style.display = "none"
+    } else {
+        list.style.display = "block"
+    }
+}
