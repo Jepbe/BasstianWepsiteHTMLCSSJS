c8c80ce0e8a620c623ab584e2989a52250786f18
Jeppe Bøgeskov <67188645+Jepbe@users.noreply.github.com>
2023-03-30 13:24:00 +0200

    69

---
+++ script/butikLinks.js
@@ -0,0 +1,66 @@
+const v1 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22218/71279/brands/clique')
+}
+
+const b1 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22219/71306/brands/clique')
+}
+
+const v2 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22215/71270/categories/shirts')
+}
+
+const b2 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22214/71268/brands/Fruit%20of%20the%20loom')
+}
+
+const v3 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22218/71334/brands/clique')
+}
+
+const b3 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22219/71333/categories/tshirts')
+}
+const v4 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22215/71271/categories/shirts')
+}
+
+const b4 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22214/71266/categories/shirts')
+}
+const v5 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22217/71278/categories/pants')
+}
+
+const b5 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22216/71273/categories/pants')
+}
+const v6 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22217/71277/brands/Fruit%20of%20the%20loom')
+}
+
+const b6 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22216/71274/categories/pants')
+}
+const v7 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/design/outfit/22221/0/brands/Fruit%20of%20the%20loom')
+}
+
+const b7 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/design/outfit/22222/0/brands/Fruit%20of%20the%20loom')
+}
+const v8 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/design/outfit/22221/0/brands/Fruit%20of%20the%20loom')
+}
+
+const b8 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/design/outfit/22222/0/brands/Fruit%20of%20the%20loom')
+}
+const v9 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22218/71281/categories/tshirts')
+}
+
+const b9 = () => {
+    window.open('https://djbasstian.sportyfied.com/da/product/22219/71313/categories/tshirts')
+}
+
